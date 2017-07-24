4c0fb5f48e05dac8fd16141599a5557d91b15936
andy3j <andy3j@gmail.com>
2017-07-25 00:53:22 +1200

    fix layout for lower resolutions

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="row">
       <h2 class="text-center">Maintenance Pages</h2>
-      <div class="col-xs-6 col-xs-offset-3">
+      <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
           <PageList filter="Env1"/>
       </div>
-      <div class="col-xs-6 col-xs-offset-3">
+      <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
           <PageList filter="Env2"/>
       </div>
-      <div class="col-xs-6 col-xs-offset-3">
+      <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
           <PageList filter="Env3"/>
       </div>
-      <div class="col-xs-2 col-xs-offset-5">
-      <button class="btn btn-block btn-primary pages-save">Save</button>
+      <div class="col-xs-12 col-sm-4 col-md-2 col-sm-offset-4 col-md-offset-5">
+      <button class="btn btn-lg btn-block btn-primary pages-save">Save</button>
     </div>
   </div>
 </template>
